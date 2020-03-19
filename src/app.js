e97222c3545c611ff9b2a41278f2b76d816505ef
nurdin <nurdin.reverse73@gmail.com>
2020-03-19 14:32:44 +0700

    sisa close browser

--- src/app.js
+++ src/app.js
@@ -23,17 +23,12 @@ const Waitlist = new WaitlistService({
 
 const connections = []
 
-const dirQueue = []
-let dataKey = []
-let keys
 io.sockets.on(`connection`,socket => {
   connections.push(socket)
   console.log(`socket connected : ${connections.length}`)
-
   socket.on('disconnect',(data) => {
     connections.splice(connections.indexOf(socket),1)
     console.log(`socket disconnect ${connections.length}`)
-    
   })  
   
   socket.on('deleteGrantAccess', async data => {
@@ -42,16 +37,15 @@ io.sockets.on(`connection`,socket => {
     let manipulateKeys = ManipulateString.changeText({text: data.redisKey,to : 'waitinglist:queue'});
     let newGranted = await Waitlist.addToGrantAccess(data)
     socket.emit('onDeleteGrantAccess',{ ...data }) 
-    socket.emit('checkPosition',{redisKey: manipulateKeys})
-    socket.broadcast.emit('checkPosition',{redisKey: manipulateKeys})
+    // socket.emit('checkPosition',{redisKey: manipulateKeys})
+    // socket.broadcast.emit('checkPosition',{redisKey: manipulateKeys})
     socket.broadcast.emit('onDeleteGrantAccess',{ redisKey : manipulateKeys, granted : newGranted})
     // console.log({manipulateKeys, newGranted,redisKey : data.redisKey});
     
   })
   socket.on('checkPosition', async data => {
     let list = await Waitlist.checkPosition(data)
-    let position = list.indexOf(data.key) + 1
-    let times = (data.time * 60 * position)
+    
     socket.emit('onCheckPosition',list)
     socket.broadcast.emit('onCheckPosition',list)
   })
@@ -72,36 +66,43 @@ io.sockets.on(`connection`,socket => {
     return result;
   })
 
-  const listData = async (redisKey) => {
-    let data = await Redis.findAll(redisKey)
-    let key = Object.keys(data)
-    for (let i = 0; i < dirQueue.length; i++) {
-      const element = dirQueue[i];
-      const find = key.find(el => el !== element)
-      dataKey.push(find)
-    }
-    return false
-  }
-  listData(keys)
-
-  const resetUser = async () => {
-    let result
-    for (let i = 0; i < dataKey.length; i++) {
-      const element = dataKey[i];
-      result = await Redis.destroy({redisKey: keys, key: element})
-    }
-    return result
-  }
+  // const listData = async (redisKey) => {
+  //   let data = await Redis.findAll(redisKey)
+  //   let key = Object.keys(data)
+  //   for (let i = 0; i < dirQueue.length; i++) {
+  //     const element = dirQueue[i];
+  //     const find = key.find(el => el !== element)
+  //     dataKey.push(find)
+  //   }
+  //   return false
+  // }
   
-  let interval = setInterval(resetUser, 10000);
-  socket.on('checkUser', data => {
-    dirQueue.push(data.key)
-    keys = data.redisKey
-    if(data !== undefined) {
-      clearInterval(interval)
-      interval = setInterval(resetUser, 10000)
-    }
-  })
+  // const resetUser = async () => {
+  //   let result
+  //   for (let i = 0; i < dataKey.length; i++) {
+  //     const element = dataKey[i];
+  //     result = await Redis.destroy({redisKey: keys, key: element})
+  //   }
+  //   return result
+  // }
+  
+  // let interval = setInterval(resetUser, 10000);
+  // socket.on('checkUser', async data => {
+  //   dirQueue.push(data.key)
+  //   keys = data.redisKey
+  //   let datas = await Redis.findAll(data.redisKey)
+  //   let key = Object.keys(datas)
+  //   for (let i = 0; i < dirQueue.length; i++) {
+  //     const element = dirQueue[i];
+  //     const find = key.find(el => el !== element)
+  //     dataKey.push(find)
+  //   }
+  //   // await listData(data.redisKey)
+  //   if(data !== undefined) {
+  //     clearInterval(interval)
+  //     interval = setInterval(resetUser, 10000)
+  //   }
+  // })
 })
 
 
